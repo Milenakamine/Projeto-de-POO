a95d40d4ba8681c6e97daeae05a8d97e4759d85e
MilenaAkamine <milenaogura@gmail.com>
2020-05-22 19:05:00 -0300

    Primeiro projeto de POO finalizado

---
+++ Personagem.cs
@@ -0,0 +1,16 @@
+namespace Primeiro_POO
+{
+    public class Personagem
+    {
+        public string nome = "Riri Williams";
+
+        public int idade = 18;
+
+        public string armadura;
+        
+        public string Atacar ()
+        {
+            return "Riri atacou!";
+          } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Primeiro_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Personagem riri = new Personagem();
+           riri.armadura= "Iron Heart";
+
+           Console.WriteLine(riri.nome);
+
+           Console.WriteLine(riri.armadura);
+
+           Console.WriteLine(riri.Atacar());
+        }
+    }
+}
